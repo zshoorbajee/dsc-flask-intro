6af718efbc52959c3c8a89787df8e37fc4e9105b
Zaid Shoorbajee <zshoorbajee@gmail.com>
2022-06-03 20:15:09 -0400

    do lesson

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello, world!'
+
+@app.route('/welcome', methods=['GET'])
+def welcome():
+    return '<b>Welcome to an amazing Flask app!</b>'
+
+@ app.route('/goodbye', methods=['GET'])
+def goodbye():
+    return 'Thanks for looking around, I guess. Come back again soon. Or don\'t?'
+
+@ app.route('/multiply', methods=['GET'])
+def multiply():
+    first=int(input('first'))
+    second=int(input('second'))
+    return str(first * second)
